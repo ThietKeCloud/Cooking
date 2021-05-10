571ef5f9355adc4c1fcb03db6a4a2962aeeae0e0
huynhthinhi <huynhthinhi206@gmail.com>
2021-05-10 11:33:33 +0700

    api index

--- Controller/recipeController.js
+++ Controller/recipeController.js
@@ -2,30 +2,72 @@ const Recipe = require('../model/Recipe');
 const USer = require('../model/User');
 const Category = require('../model/Category');
 const User = require('../model/User');
+const db = require('../db');
 const controller = {};
 
-// Trending Product
+
+// Trending recipe
+const trending = `select "Recipes".*, "Categories".namecategory, "Users".username from ("Recipes" join "Categories" on "Recipes"."categoryId" ="Categories".id) join "Users"
+on "Recipes"."teacherID" = "Users".id 
+where "Users".role = 1 AND "Recipes".lock != 1
+order by rating DESC
+LIMIT 10`;
+
+
+
 controller.trendingRecipe = ()=>{
     
-    // return new Promise((resolve, reject)=>{
-    //     Recipe.findAll({
-            
-    //         include: {
-    //           model: User,
-    //           where: {teacherID = User.id}  
-    //         }
-    //       });
-       
-    // })
+    return new Promise((resolve, reject)=>{
+        
+       db.query(trending)
+       .then((result) => {
+           
+           resolve(result[0]);
+       }).catch((error) => {
+           console.log("error at recipe controller");
+       });
+    })
 }
 
+
+const lastest =  `select "Recipes".*, "Categories".namecategory, "Users".username from ("Recipes" join "Categories" on "Recipes"."categoryId" ="Categories".id) join "Users"
+on "Recipes"."teacherID" = "Users".id 
+where "Users".role = 1 AND "Recipes".lock != 1
+order by "createdAt" DESC
+LIMIT 10`
 controller.lastestRecipe = ()=>{
 
+    return new Promise((resolve, reject)=>{
+        db.query(lastest)
+        .then((result)=>{
+            resolve(result[0]);
+        })
+        .catch((error)=>{
+            console.log("error lastest at recipe controller");
+        })
+    })
 }
 
+// best seller 
+const bestSeller = `select "Recipes".*, "Categories".namecategory, "Users".username from ("Recipes" join "Categories" on "Recipes"."categoryId" ="Categories".id) join "Users"
+on "Recipes"."teacherID" = "Users".id 
+where "Users".role = 1 AND "Recipes".lock != 1
+order by "numberBuyers" DESC
+LIMIT 10`
 controller.bestSellers = ()=>{
-    
+    return new Promise((resolve, reject)=>{
+        db.query(bestSeller)
+        .then((result)=>{
+            resolve(result[0]);
+        })
+        .catch((error)=>{
+            console.log("error sellers at recipe controller");
+        })
+    })
 }
+
+
+
 
 
 module.exports = controller;
--- Router/indexRoute.js
+++ Router/indexRoute.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const router = express.Router();
 const recipeController = require('../Controller/recipeController');
+const categoryController = require('../Controller/categoryController');
 
 
 router.get('/', (req, res, next)=>{
     
-   recipeController.trendingRecipe()
-   .then((result)=>{
-       res.send(result);
+   categoryController.getCategory()
+   .then((category)=>{
+    recipeController.trendingRecipe()
+    .then((trending)=>{
+        recipeController.bestSellers()
+        .then((sellers)=>{
+            recipeController.lastestRecipe()
+            .then((lastest)=>{
+                res.json({
+                    category: category,
+                    trending: trending,
+                    sellers: sellers,
+                    lastest: lastest
+                })
+               
+            })
+            .catch((error)=>{
+                console.log("error lastest at indexRoute");
+            })
+            
+        })
+        .catch((error)=>{
+            console.log("error seller at indexRoute");
+        })
+    })
+    .catch((error)=>{
+        console.log(err);
+        console.log("error trending at indexRoute");
+    })
    })
-   .catch((err)=>{
-       console.log("error at index Route");
+   .catch((error)=>{
+       console.log("error category at index Route");
    })
    
 });
