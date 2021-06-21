b00c833b2f181e9b8aee291c9c96995d108e9401
Hồ Hoàng Việt Tiến <50546161+hotien2107@users.noreply.github.com>
2021-06-21 21:41:18 +0700

    fix index

--- WebNodejs/db.js
+++ WebNodejs/db.js
@@ -3,11 +3,8 @@ const sequelize = require('sequelize')
 const db = new sequelize({
     database: "Cook",
     username: "postgres",
-<<<<<<< HEAD
-    password: "vuivelavang",
-=======
+
     password: "Hotien2107",
->>>>>>> e075c85c21443840cc900f7bc8166be3a97cc59d
     host: "localhost",
     port: 5432,
     dialect: "postgres",
