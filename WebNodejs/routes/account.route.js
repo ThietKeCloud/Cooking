b685971e0133fff61a5ddaf4915bc726abb6cf6c
HuyThien <57531802+HuyThien@users.noreply.github.com>
2021-06-29 16:05:07 +0700

    edit register

--- WebNodejs/routes/account.route.js
+++ WebNodejs/routes/account.route.js
@@ -82,15 +82,13 @@ router.post("/register", async function (req, res) {
     role: 0,
     check: 0
   };
-
   await userModel.add(user);
-
   let transporter = nodemailer.createTransport({ // config mail server
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
     auth: {
-      user: 'academynhom6@gmail.com', //Tài khoản gmail vừa tạo
+      user: 'happycookcloud@gmail.com', //Tài khoản gmail vừa tạo
       pass: '1357abcd' //Mật khẩu tài khoản gmail vừa tạo
     },
     tls: {
@@ -103,11 +101,11 @@ router.post("/register", async function (req, res) {
   link = "http://" + req.get('host') + "/verify?id=" + user.Name;
   // console.log(link);
   var mainOptions = { // thiết lập đối tượng, nội dung gửi mail
-    from: 'Online Academy',
+    from: 'Happy Cooking',
     to: user.Email,
     text: 'You recieved message from ',
-    subject: "Activate an Online Academy account",
-    html: "<h3>Hello, " + `${user.Name}` + "</h3><h4>We are very happy because you have signed up for online Academy website,<br>Please Click on the button to verify your email.</h4><a style='background-color:mediumblue;color:white;text-decoration:none;padding:10px 20px;border-radius:5px; margin-left: 180px; margin-bot:10px' href=" + link + ">Verify</a><br><br><h2>-------------------------------------------------------------------------</h2><h5>This is email automatically sent from ThienNhiTam Team.<br>Please do not reply to this email.</h5>"
+    subject: "Activate an Happy Cooking account",
+    html: "<h3>Hello, " + `${user.Name}` + "</h3><h4>We are very happy because you have signed up for Happy Cooking website,<br>Please Click on the button to verify your email.</h4><a style='background-color:mediumblue;color:white;text-decoration:none;padding:10px 20px;border-radius:5px; margin-left: 180px; margin-bot:10px' href=" + link + ">Verify</a><br><br><h2>-------------------------------------------------------------------------</h2><h5>This is email automatically sent from ThienNhiTam Team.<br>Please do not reply to this email.</h5>"
   }
 
   transporter.sendMail(mainOptions, function (err, info) {
