28aead3293c829815968e7efcd21faa9cae973c9
Hồ Hoàng Việt Tiến <50546161+hotien2107@users.noreply.github.com>
2021-06-29 20:05:13 +0700

    change db name

--- WebNodejs/db.js
+++ WebNodejs/db.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const db = new sequelize({
-    database: "Cooking",
+    database: "Cook",
     username: "postgres",
 
     password: process.env.password,
