2cfb9021514ac12db05a938d1818fc52b1744243
HuyThien <57531802+HuyThien@users.noreply.github.com>
2021-07-02 17:20:25 +0700

    edit file .env

--- WebNodejs/db.js
+++ WebNodejs/db.js
@@ -4,14 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const db = new sequelize({
-    database: "Cook",
+    database: process.env.database,
     username: "postgres",
-<<<<<<< HEAD
-    password: 'vuivelavang',
-=======
-
     password: process.env.password,
->>>>>>> 67c84598a403f039230dc2a3b589ed3a5c07e96d
+
     host: "localhost",
     port: 5432,
     dialect: "postgres",
