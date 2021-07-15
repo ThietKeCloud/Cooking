7f5c394310a7c5a57ab1ed892883780ab53887c0
HuyThien <57531802+HuyThien@users.noreply.github.com>
2021-07-15 13:16:04 +0700

    Update favorite.route.js

--- WebNodejs/routes/favorite.route.js
+++ WebNodejs/routes/favorite.route.js
@@ -3,36 +3,36 @@ const Like = require('../models/Like');
 const router = express.Router();
 
 
-router.get('/',(req,res,next)=>{
+router.get('/', (req, res, next) => {
     favorite = require('../Controllers/favariteController');
-    
-    favorite.getFavoriteList( req.session.user.id).then(favorite=>{
+
+    favorite.getFavoriteList(req.session.user.id).then(favorite => {
         //console.log(favorite[0], favorite[0].length);
-        res.render('Favorite',{banner:'Danh sách công thức yêu thích', favorite : favorite[0], check : favorite[0].length === 0 , totalFav: favorite[0].length})
+        res.render('Favorite', { banner: 'Danh sách công thức yêu thích', favorite: favorite[0], check: favorite[0].length === 0, totalFav: favorite[0].length })
     })
 
 });
 
-router.post('/',(req,res,next)=>{
-    
-    if(req.session.user){
-        var courseId = req.body.id; 
+router.post('/', (req, res, next) => {
+
+    if (req.session.user) {
+        var courseId = req.body.id;
         var userid = req.session.user.id;
         var favorite = require('../Controllers/favariteController');
-        favorite.getCoures(courseId).then(data=>{      
-           Like.create({courseid: courseId, userid: userid});
-        })
-        
+
+        Like.create({ courseid: courseId, userid: userid });
+
+
     }
 
 });
 
-router.delete('/',(req,res)=>{
+router.delete('/', (req, res) => {
     var courseId = req.body.id;
     console.log(courseId);
     Like.destroy({
-        where: {courseid: courseId, userid: req.session.user.id, totalFav: favorite[0].length}
+        where: { courseid: courseId, userid: req.session.user.id, totalFav: favorite[0].length }
     })
-}); 
+});
 
 module.exports = router;
